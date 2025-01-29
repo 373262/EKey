982993cce316ceaedebd98c4f7b22a438744533a
Anton <mezhmidinov@gmail.com>
2025-01-29 16:19:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -452,7 +452,8 @@ def main(disable_exit=False):
                      photo_attachment = 'photo-203143822_457239283'
                     bot.send_message(-1002475137672, output_line + activate_products +  "\n\n" + please_comment +"\n\n[⚡️Накидать бустов\!](https://t\.me/boost/mynod32) \| [\@mynod32](https://t\.me/\+wLqOncLmqAIwZGM6)" + hashtags, disable_web_page_preview=True, disable_notification=True)
                     bot.send_message(-1001233475775, output_line + activate_products +  "\n\n" + please_comment +"\n\n[⚡️Накидать бустов\!](https://t\.me/boost/mynod32) \| [\@mynod32](https://t\.me/\+wLqOncLmqAIwZGM6)" + hashtags, disable_web_page_preview=True, disable_notification=True)  
-                    vk.wall.post(owner_id=vk_group_id_value, message=activate_products_vk + output_line_vk + "\n\n" + vk_end , attachments=photo_attachment, donut_paid_duration=1296000)
+                    #vk.wall.post(owner_id=vk_group_id_value, message=output_line_vk + "\n\n" + vk_end , attachments=photo_attachment, donut_paid_duration=1296000)
+                    vk.wall.post(owner_id=vk_group_id_value, message=output_line_vk + "\n\n" + vk_end , attachments=photo_attachment)
                     vk2.wall.post(owner_id=-229183047, message=activate_products_vk + output_line_vk + "\n\n" + vk_end, attachments=photo_attachment)
                     if args['vpn_codes']:
                         EV_obj = EV(email_obj, driver, ER_obj.window_handle)
@@ -482,7 +483,8 @@ def main(disable_exit=False):
                             output_line_vk = f'\n🛡 Продукт: ESET VPN\n🕐 Срок действия: {license_out_date}\n\n{license_keys_formatted_vk}\n'
                             bot.send_message(-1002475137672, output_line + please_comment +"\n\n[⚡️Накидать бустов\!](https://t\.me/boost/mynod32) \| [\@mynod32](https://t\.me/\+wLqOncLmqAIwZGM6)" + hashtags, disable_web_page_preview=True, disable_notification=True)
                             bot.send_message(-1001233475775, output_line + please_comment +"\n\n[⚡️Накидать бустов\!](https://t\.me/boost/mynod32) \| [\@mynod32](https://t\.me/\+wLqOncLmqAIwZGM6)" + hashtags, disable_web_page_preview=True, disable_notification=True)
-                            vk.wall.post(owner_id=vk_group_id_value, message=output_line_vk + "\n\n" + vk_end , attachments=photo_attachment, donut_paid_duration=1296000)
+                            #vk.wall.post(owner_id=vk_group_id_value, message=output_line_vk + "\n\n" + vk_end , attachments=photo_attachment, donut_paid_duration=1296000)
+                            vk.wall.post(owner_id=vk_group_id_value, message=output_line_vk + "\n\n" + vk_end , attachments=photo_attachment)
                             vk2.wall.post(owner_id=-229183047, message=output_line_vk + "\n\n" + vk_end, attachments=photo_attachment)
             # ESET ProtectHub
             elif args['protecthub_account'] or args['advanced_key']:
@@ -531,7 +533,8 @@ def main(disable_exit=False):
                      photo_attachment = 'photo-203143822_457239283'
                      bot.send_message(-1002475137672, output_line +  "\n\n" + please_comment +"\n\n[⚡️Накидать бустов\!](https://t\.me/boost/mynod32) \| [\@mynod32](https://t\.me/\+wLqOncLmqAIwZGM6)" + hashtags, disable_web_page_preview=True, disable_notification=True)
                      bot.send_message(-1001233475775, output_line +  "\n\n" + please_comment +"\n\n[⚡️Накидать бустов\!](https://t\.me/boost/mynod32) \| [\@mynod32](https://t\.me/\+wLqOncLmqAIwZGM6)" + hashtags, disable_web_page_preview=True, disable_notification=True)
-                     vk.wall.post(owner_id=vk_group_id_value, message=output_line_vk + "\n\n" + vk_end , attachments=photo_attachment, donut_paid_duration=1296000)
+                     #vk.wall.post(owner_id=vk_group_id_value, message=output_line_vk + "\n\n" + vk_end , attachments=photo_attachment, donut_paid_duration=1296000)
+                     vk.wall.post(owner_id=vk_group_id_value, message=output_line_vk + "\n\n" + vk_end , attachments=photo_attachment)
                      vk2.wall.post(owner_id=-229183047, message=output_line_vk + "\n\n" + vk_end, attachments=photo_attachment)
             # end
             console_log(output_line)
